1aa0a7bfe73c4fa8cd62baf022b9a776f8dcb18f
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-08-01 16:43:14 +0100

    styles added

--- UsePopCorn/src/starRating.js
+++ UsePopCorn/src/starRating.js
@@ -1,7 +1,18 @@
+const containerSyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '1.2rem',
+};
+const starStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.25rem',
+};
+
 export default function StarRating() {
   return (
-    <div className="star--rating">
-      <div>
+    <div style={containerSyle}>
+      <div style={starStyle}>
         {Array.from({length: 10}, (_, i) => (
           <span key={i}>s{i + 1}</span>
         ))}
