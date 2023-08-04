1cf59af0ffd04fe867f817214cb7d3a5007c2d1d
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-08-04 19:46:45 +0100

    STAR COMPONENT ADDED

--- UsePopCorn/src/index.js
+++ UsePopCorn/src/index.js
@@ -11,6 +11,7 @@ root.render(
     <StarRating
       maxRating={20}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+      size={2.25}
     />
     <StarRating maxRating={15} color="green" size={2} defaultRating={3} />
     {/* <StarRating
